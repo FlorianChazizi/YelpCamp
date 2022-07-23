55303f0971ab3767bc23b9c9a9ae523b13697395
unknown <giannis1944@hotmail.com>
2022-07-24 01:48:29 +0300

    add start

--- app.js
+++ app.js
@@ -141,6 +141,8 @@ app.all('*', (req,res,next) => {
     next(new ExpressError('Page not found', 404))
 })
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port :${port}`)
 })
